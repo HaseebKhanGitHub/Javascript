318d33b77606b757393632467281935f3cdd6851
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-07-07 22:08:33 +0500

    Create script.js

---
+++ 4/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+function a() {
+	// body...
+	return{
+		name:"haseeb"
+	};
+}
+
+console.log(a());
+
+//mistake in return braces
+
+function b() {
+	// body...
+	return
+	{
+		name:"haseeb"
+	};
+}
+
+console.log(b());
