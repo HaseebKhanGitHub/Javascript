12ce26068d4904aeb37cbaedc3e91a1b15f74db3
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-07-11 23:47:33 +0500

    Create script.js

---
+++ 7/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+var facebook={
+	name:"Facebook",
+	ceo:{
+		fname:"Mark",
+		lname:"Zucerberg"
+	},
+	language:{
+		frontend:"html"
+	},
+	// $stock:1100
+	"stock new price":1111
+};
+// console.log(facebook);
+console.log(facebook.ceo.lname);
