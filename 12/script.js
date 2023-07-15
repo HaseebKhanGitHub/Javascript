a27e03a93b7528a8da69f62e6fb90d84705e4ef4
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-07-15 13:17:28 +0500

    Create script.js

---
+++ 12/script.js
@@ -0,0 +1,42 @@
+
+
+
+
+/*
+var array= new Array();
+array[0]="haseeb";
+array[1]=3;
+array[2]= function(name) {
+	console.log("Hi "+name);
+};
+array[3]={course:"html, css and js"};
+
+console.log(array);
+array[2](array[0]);
+console.log(array[3].course);
+*/
+
+
+
+//short hand array creation
+var names=[
+	//{name: "haseeb"},
+	"Haseeb","khan","Qureshi"
+	]
+// console.log(names);
+
+/*
+for(var i=0; i<names.length; i++){
+	console.log("Hello "+names[i]);
+}
+*/
+
+names[100]="lada";
+for(var i=0; i<names.length; i++){
+	console.log("Hello "+names[i]);
+}
+
+
+
+
+
