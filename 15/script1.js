65c08009b935fc78518f8026d56ad917584d7a12
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-07-18 23:09:16 +0500

    Create script1.js

---
+++ 15/script1.js
@@ -0,0 +1,23 @@
+//wrong
+var name="Haseeb";
+function sayHello(){
+	console.log("hello: "+name);
+}
+
+
+
+
+
+//right
+
+
+/*
+var haseebGreeter= {};
+haseebGreeter.name="Haseeb";
+haseebGreeter.sayHello=function() {
+
+	console.log("hello: "+haseebGreeter.name);
+}
+
+*/
+
