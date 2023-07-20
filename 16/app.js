d55b17c4659800c3a9a835160357b6ccec474b29
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2023-07-20 10:43:33 +0500

    Create app.js

---
+++ 16/app.js
@@ -0,0 +1,18 @@
+
+
+//right
+
+haseebGreeter.sayHello();
+johnGreeter.sayHi();
+
+
+
+/*
+	immediately invoked function expression
+
+(function (name) {
+	console.log("hello "+name);
+
+}("Haseeb"));
+
+*/
